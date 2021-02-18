449a50f3d9c849f6a69181025123244e758971c9
Fx <fx73000@yahoo.fr>
2021-02-18 18:13:35 +0100

    fix

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -10,7 +10,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class ChatClient {
-	static Room room = null;
+	static ChatRoom room = null;
 	static Hub hub;
 
 	public static void main(String[] args) {
