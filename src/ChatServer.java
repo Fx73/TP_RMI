f8f1506a0e7c4f0b2ac2e3314fbbdbf5e2d6d1aa
Fx <fx73000@yahoo.fr>
2021-02-18 17:34:41 +0100

    fix

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -12,7 +12,7 @@ public class ChatServer {
 		try {
 		  // Create a ChatRoom remote object
 	    ChatHub hub = new ChatHub ();
-	    Room hub_stub = (Room) UnicastRemoteObject.exportObject(hub, 0);
+	    Hub hub_stub = (Hub) UnicastRemoteObject.exportObject(hub, 0);
 
 	    // Register the remote object in RMI registry with a given identifier
 	    Registry registry= LocateRegistry.getRegistry(); 
