15064ed74c047ab48b2fb0faa94b10455f1de252
Fx <fx73000@yahoo.fr>
2021-02-26 13:12:23 +0100

    timeout

--- src/ChatRoom.java
+++ src/ChatRoom.java
@@ -1,11 +1,14 @@
 import java.io.Serializable;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ChatRoom implements Room, Serializable {
     ChatLog _chatlog;
     String _name;
     ArrayList<String> users = new ArrayList<>();
+    ArrayList<Timer> timers = new ArrayList<>();
 
     ChatRoom(String name){
         _name = name;
@@ -26,6 +29,14 @@ public class ChatRoom implements Room, Serializable {
 
     public void Register_User(String name){
         users.add(name);
+        Timer timer = new Timer(name);
+        timers.add(timer);
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                AutoUnregister_User(name);
+            }
+        }, 10000, 10000);
     }
 
     public void Unregister_User(String name){
@@ -34,5 +45,21 @@ public class ChatRoom implements Room, Serializable {
     public String[] Get_Users(){
         return users.toArray(new String[0]);
     }
+
+    private TimerTask AutoUnregister_User(String name){
+        timers.remove(users.indexOf(name));
+        users.remove(name);
+        return null;
+    }
+}
+
+class User{
+    String _name;
+    Timer _timer;
+    public User(String name) {
+        _name = name;
+        _timer = new Timer();
+    }
+
 
 }
